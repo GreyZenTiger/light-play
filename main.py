098995ec6b3378f73fd77909b6b938223edd7ef5
Julian-Roy Pätz <julian-roy.paetz@outlook.de>
2020-12-07 10:06:14 +0100

    Implemented a second mode and the ability to chose between the first and the second one

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 # coding: utf8
 
+import sys
 import time
+from random import random, randint
 from typing import List
 
 import RPi.GPIO as GPIO
 
 # defines numbers of GPIO pins
 GPIO.setmode(GPIO.BOARD)
+
+# the mode chosen by the user
+mode: int = int(sys.argv[1])
 
 # defines led's
 led0: int = 16
@@ -17,6 +22,7 @@ led3: int = 11
 led4: int = 13
 led5: int = 15
 
+# list of all defined led's
 led_list: List[int] = [led0, led1, led2, led3, led4, led5]
 
 # defines GPIO pins as outputs
@@ -36,8 +42,22 @@ def running_light(runs: int = 3, wait: float = 0.1):
             GPIO.output(int(led), GPIO.LOW)
             time.sleep(wait)
         runs = runs - 1
-    GPIO.cleanup()
+
+
+def flickering_light(duration: int = 50):
+    while duration > 0:
+        random_led = randint(0, 5)
+        GPIO.output(int(led_list[random_led]), GPIO.HIGH)
+        time.sleep(random())
+        GPIO.output(int(led_list[random_led]), GPIO.LOW)
+        duration = duration - 1
 
 
 if __name__ == '__main__':
-    running_light()
+    if mode == 1:
+        running_light()
+    elif mode == 2:
+        flickering_light()
+    else:
+        sys.stdout.write("Please choose a mode between 1 and 2.")
+    GPIO.cleanup()
