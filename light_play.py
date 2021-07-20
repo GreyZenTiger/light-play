59d037ad4e538ef89d3b39b955935bd2307c1699
GreyZenTiger <julian.paetz97@icloud.com>
2021-07-20 12:17:10 +0200

    Released v0.2-alpha
    Features
    - added a config file to configure LED amount and position (GPIO pin)
    - added possibility to connect more or less LEDs
    - added possibility to connect the LEDs to different GPIO pins
    
    Various Information
    - changed the initial order of the LEDs
    
    BugFixes
    - /

--- light_play.py
+++ light_play.py
@@ -8,38 +8,56 @@ light animations on a Raspberry Pi.
 
 Functions:
 
+create_config() -- creates config.ini if it's not existing
+load_config() -- loads all variables from config.ini
 knight_rider_light() -- a light running from one side to the other and back
 flickering_light() -- the LEDs flicker randomly
 clapping_light() -- two lights running from the outer LEDs to the inner ones
 """
 
+import os.path
 import sys
 import time
+from configparser import ConfigParser
 from random import random, randint
 from typing import List
 
 import RPi.GPIO as GPIO
 
+# get configparser object
+config = ConfigParser()
+config.read('config.ini')
+# list of all defined LEDs
+led_list: List[int] = []
 # defines how the GPIO pins are count
 GPIO.setmode(GPIO.BOARD)
 # the mode chosen by the user
-mode: int = int(sys.argv[1])
-# defines to which GPIO pins the LEDs are connected
-led0: int = 16
-led1: int = 18
-led2: int = 22
-led3: int = 11
-led4: int = 13
-led5: int = 15
-# list of all defined LEDs
-led_list: List[int] = [led0, led1, led2, led3, led4, led5]
-# defines GPIO pins as outputs
-GPIO.setup(led0, GPIO.OUT)
-GPIO.setup(led1, GPIO.OUT)
-GPIO.setup(led2, GPIO.OUT)
-GPIO.setup(led3, GPIO.OUT)
-GPIO.setup(led4, GPIO.OUT)
-GPIO.setup(led5, GPIO.OUT)
+mode: int = int(sys.argv[1])    # TODO initialization raises an error if no sys.argv was given
+
+
+def create_config():
+    """Creates the config.ini file if it's not existing."""
+    # check if config.ini exists
+    if not os.path.isfile('config.ini'):
+        # predefine content of config.ini
+        config['LedPosition'] = {
+            'led0': '16', 'led1': '18', 'led2': '22',
+            'led3': '36', 'led4': '38', 'led5': '40'
+        }
+        # create config.ini
+        with open('config.ini', 'w') as conf:
+            config.write(conf)
+
+
+def load_config():
+    """Loads all variables from config.ini file."""
+    global led_list
+    # load led positions from config.ini
+    for key in config['LedPosition']:
+        led_list.append(config.getint('LedPosition', key))
+    # defines used GPIO pins as outputs
+    for led in led_list:
+        GPIO.setup(led, GPIO.OUT)
 
 
 def knight_rider_light(runs: int = 10, wait: float = 0.1):
@@ -71,10 +89,12 @@ def flickering_light(duration: int = 50):
     the LEDs would flash. (default is 50)
     """
     while duration > 0:
-        random_led = randint(0, 5)
-        GPIO.output(int(led_list[random_led]), GPIO.HIGH)
+        # chooses a random index
+        random_index = randint(0, len(led_list) - 1)
+        # flash random LED
+        GPIO.output(int(led_list[random_index]), GPIO.HIGH)
         time.sleep(random())
-        GPIO.output(int(led_list[random_led]), GPIO.LOW)
+        GPIO.output(int(led_list[random_index]), GPIO.LOW)
         duration = duration - 1
 
 
@@ -91,13 +111,15 @@ def clapping_light(runs: int = 10, wait: float = 0.1, invert: bool = False):
     :parameter invert bool, optional Describes if the flashing lights
     should start at the outer or the inner LEDs. (default is False)
     """
-    # splitting the list of all LEDs in outer, middle and inner LEDs
-    outer_led: List[int] = [led_list[0], led_list[5]]
-    middle_led: List[int] = [led_list[1], led_list[4]]
-    inner_led: List[int] = [led_list[2], led_list[3]]
+    left_index: int = 0
+    right_index: int = len(led_list) - 1
     # holds the single LED pairs starting with outer LEDs at index 0
-    led_pairs: List[List] = [outer_led, middle_led, inner_led]
-
+    led_pairs: List[List] = []
+    # splitting the list of all LEDs in pairs from outer to inner LEDs
+    while left_index <= right_index:
+        led_pairs.append([led_list[left_index], led_list[right_index]])
+        left_index += 1
+        right_index -= 1
     while runs > 0:
         if not invert:
             # starts flashing with outer LEDs
@@ -119,6 +141,8 @@ def clapping_light(runs: int = 10, wait: float = 0.1, invert: bool = False):
 
 
 if __name__ == '__main__':
+    create_config()
+    load_config()
     if mode == 1:
         knight_rider_light()
     elif mode == 2:
@@ -127,4 +151,5 @@ if __name__ == '__main__':
         clapping_light()
     else:
         sys.stdout.write("Please choose a mode between 1 and 3.")
-    GPIO.cleanup()
+    # cleanup the used GPIO pins
+    GPIO.cleanup(led_list)
